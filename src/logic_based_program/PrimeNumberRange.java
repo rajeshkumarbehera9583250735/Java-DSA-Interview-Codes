6590f03c9391c00f6394e305bff36f536d8036b4
Rajesh Kumar Behera <rajeshkumarbeheracool@gmail.com>
2025-07-05 17:36:02 +0530

    Print all prime numbers in a user-defined range.

---
+++ src/logic_based_program/PrimeNumberRange.java
@@ -0,0 +1,58 @@
+/*
+ 
+  
+Print all Prime Numbers within a Given Range
+Problem Statement:
+
+Write a Java program that accepts two integers from the user (start and end of a range) and prints all prime numbers within that range, inclusive.
+
+Example:
+Input:
+Start = 10
+End = 30
+
+Output:
+Prime numbers between 10 and 30 are: 11, 13, 17, 19, 23, 29
+ 
+ 
+*/
+package logic_based_program;
+
+import java.util.Scanner;
+
+public class PrimeNumberRange {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Enter Starting value:");
+		int start= sc.nextInt();
+		System.out.println("Enter Ending value:");
+		int end= sc.nextInt();
+		
+		boolean isPrime=true;
+		for(int i =start;i<=end; i++) {
+			
+			if(i <= 1) {
+				continue;
+			}
+			
+			isPrime=true;
+			
+			for(int j=2;j<= Math.sqrt(i);j++) {
+				 if(i % j == 0) {
+					 isPrime=false;
+				 }
+			}
+			
+			if(isPrime) {
+				System.out.println(i + " is a prime number.");
+			}else {
+				System.out.println(i + " is not a prime number.");
+			}
+			
+		}
+		sc.close();
+	}
+
+}
